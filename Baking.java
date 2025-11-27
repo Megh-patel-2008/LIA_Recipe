3d21b4561976a25a43ff56a33e43d553847f0aef
Nathaniel <2541428@edu.vaniercollege.qc.ca>
2025-11-27 14:07:48 -0500

    changed a method in baking

--- Baking.java
+++ Baking.java
@@ -25,9 +25,8 @@ public class Baking extends Recipe_Book
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
+    public int sampleMethod(int z)
     {
-        // put your code here
-        return x + y;
+        return z;
     }
 }
