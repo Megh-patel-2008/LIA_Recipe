a3af8059fa835fc90d9432ed7e98ac053f4d2d0b
Megh Patel <M3ghpatel2008@gmail.com>
2025-12-01 14:13:37 -0500

    with some function but nor yet started

--- Baking.java
+++ Baking.java
@@ -6,27 +6,8 @@
  * @version (a version number or a date)
  */
 public class Baking extends Recipe_Book
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Baking
-     */
-    public Baking()
+{ // change void with String
+    public void getDetailsBaking()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int z)
-    {
-        return z;
     }
 }
--- Frying.java
+++ Frying.java
@@ -6,28 +6,8 @@
  * @version (a version number or a date)
  */
 public class Frying extends Recipe_Book
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Frying
-     */
-    public Frying()
+{ // change void with String
+    public void getDetailsFrying()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
     }
 }
