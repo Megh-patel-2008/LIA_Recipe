b825904f269b43ffd4912c7ef35af1b7b7bc7f0f
Megh Patel <M3ghpatel2008@gmail.com>
2025-12-09 13:22:42 -0500

    Baking

--- Baking.java
+++ Baking.java
@@ -7,8 +7,16 @@
  */
 public class Baking extends Recipe_Book
 
-{// change void with String
-    public void getDetailsBaking()
+{
+    private int ovenTemp;
+    private int bakeTime;
+    private boolean containsEgg;
+    
+    public Baking(int servings, int ovenTemp, int bakeTime, boolean containsEgg)
     {
+        super(servings);
+        this.ovenTemp = ovenTemp;
+        this.bakeTime = bakeTime;
+        this.containsEgg = containsEgg;
     }
 }
